10d5a29694107d778bcc35d3bca855b0181f159b
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-04-30 11:48:32 -0500

    Foods table visual improvements (colors, tags..)

--- src/views/Foods.vue
+++ src/views/Foods.vue
@@ -57,14 +57,88 @@
     </div>
     <div class="container is-fluid">
       <AddFood v-if="addFoodForm" @cancel="addFoodForm = false" />
-      <b-table
-        v-else
-        :data="data"
-        :columns="columns"
-        default-sort="food"
-        :hoverable="true"
-        :loading="isLoading"
-      ></b-table>
+      <table v-else class="table is-fullwidth is-hoverable">
+        <thead>
+          <tr>
+            <th>food</th>
+            <th class="is-size-7">qty.</th>
+            <th class="is-size-7">port.</th>
+            <th class="has-text-danger">kcal</th>
+            <th><abbr title="Protein">protein</abbr></th>
+            <th><abbr title="Carbs">carbs</abbr></th>
+            <th><abbr title="Fat">fat</abbr></th>
+            <th class="is-size-7"><abbr title="Saturated Fat">s fat</abbr></th>
+            <th class="is-size-7"><abbr title="Trans Fat">t fat</abbr></th>
+            <th class="is-size-7"><abbr title="Mono Fat">m fat</abbr></th>
+            <th class="is-size-7"><abbr title="Poly Fat">p fat</abbr></th>
+            <th class="has-text-info"><abbr title="Sugar">sugar</abbr></th>
+            <th class="is-size-7"><abbr title="Fiber">fiber</abbr></th>
+            <th class="is-size-7"><abbr title="Sodium">sodium</abbr></th>
+            <th class="is-size-7"><abbr title="Cholesterol">chol</abbr></th>
+            <th class="is-size-7"><abbr title="Vitamin B12">v12</abbr></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="food in data" :key="food">
+            <td width="300">
+              <div>{{ food.food }}</div>
+              <div class="is-size-7">
+                <strong>{{ food.brand }}</strong>
+              </div>
+              <b-taglist v-if="food.tags.length">
+                <b-tag v-for="tag in food.tags" :key="tag" type="is-info">
+                  {{ tag }}
+                </b-tag>
+              </b-taglist>
+            </td>
+            <td class="is-size-7">{{ food.qty }}</td>
+            <td class="is-size-7">{{ food.portion }}</td>
+            <td>
+              <strong class="has-text-danger">{{ food.kcal }}</strong>
+            </td>
+            <td>
+              <strong>{{ food.protein }}g</strong>
+            </td>
+            <td>
+              <strong>{{ food.carbs }}g</strong>
+            </td>
+            <td>
+              <strong>{{ food.fat }}g</strong>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.s_fat !== null">{{ food.s_fat }}g</div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.t_fat !== null">{{ food.t_fat }}g</div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.m_fat !== null">{{ food.m_fat }}g</div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.p_fat !== null">{{ food.p_fat }}g</div>
+            </td>
+            <td>
+              <div v-if="food.sugar !== null">
+                <strong class="has-text-info">{{ food.sugar }}g</strong>
+              </div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.fiber !== null">{{ food.fiber }}g</div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.sodium !== null">{{ food.sodium }}mg</div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.cholesterol !== null">
+                {{ food.cholesterol }}mg
+              </div>
+            </td>
+            <td class="is-size-7">
+              <div v-if="food.v12">{{ food.v12 }}%</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
@@ -80,24 +154,6 @@ export default {
   data() {
     return {
       data: [],
-      columns: [
-        { field: "food", label: "food" },
-        { field: "qty", label: "qty." },
-        { field: "portion", label: "portion" },
-        { field: "kcal", label: "kcal" },
-        { field: "protein", label: "protein (g)" },
-        { field: "carbs", label: "carbs (g)" },
-        { field: "fat", label: "fat (g)" },
-        { field: "s_fat", label: "s fat (g)" },
-        { field: "t_fat", label: "t fat (g)" },
-        { field: "m_fat", label: "m fat (g)" },
-        { field: "p_fat", label: "p fat (g)" },
-        { field: "sugar", label: "sugar (g)" },
-        { field: "fiber", label: "fiber (g)" },
-        { field: "sodium", label: "sodium (mg)" },
-        { field: "cholesterol", label: "cholesterol (mg)" },
-        { field: "v12", label: "v12" }
-      ],
       brands: {
         all: [],
         selected: 0
@@ -118,7 +174,8 @@ export default {
 
       foods.forEach(food => {
         this.data.push({
-          food: `${food.food_name} (${food.brand_name})`,
+          food: food.food_name,
+          brand: food.brand_name,
           qty: `${food.quantity} ${food.measure}`,
           portion: `${food.portion} ${food.unit}`,
           kcal: food.kcal,
@@ -133,7 +190,8 @@ export default {
           fiber: food.fiber,
           sodium: food.sodium,
           cholesterol: food.cholesterol,
-          v12: food.vitamin_b12
+          v12: food.vitamin_b12,
+          tags: food.tags
         });
       });
 
@@ -154,7 +212,8 @@ export default {
       api.filter(this.brands.selected, this.tags.selected).then(res => {
         res.forEach(food => {
           this.data.push({
-            food: `${food.food_name} (${food.brand_name})`,
+            food: food.food_name,
+            brand: food.brand_name,
             qty: `${food.quantity} ${food.measure}`,
             portion: `${food.portion} ${food.unit}`,
             kcal: food.kcal,
@@ -169,7 +228,8 @@ export default {
             fiber: food.fiber,
             sodium: food.sodium,
             cholesterol: food.cholesterol,
-            v12: food.vitamin_b12
+            v12: food.vitamin_b12,
+            tags: food.tags
           });
         });
       });
